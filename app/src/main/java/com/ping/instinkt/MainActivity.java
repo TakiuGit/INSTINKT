b5fd4dec96e7a29ff5ccf0f809cb77d21b6d701d
TakiuGit <paulnoferi@gmail.com>
2015-03-13 16:08:59 +0100

    First try

--- app/src/main/java/com/ping/instinkt/MainActivity.java
+++ app/src/main/java/com/ping/instinkt/MainActivity.java
@@ -1,17 +1,33 @@
 package com.ping.instinkt;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    TextView tv_test;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bDist= (Button) findViewById(R.id.button1);
+        bDist.setOnClickListener(this);
+
+        Button bVib = (Button) findViewById(R.id.button2);
+        bVib.setOnClickListener(this);
+
+        tv_test = (TextView)  findViewById(R.id.test);
     }
 
 
@@ -35,5 +51,19 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId())
+        {
+            case R.id.button1 :
+                tv_test.setText("Calcul Distance");
+                break;
+            case R.id.button2 :
+                tv_test.setText("Vibration");
+                break;
+        }
     }
 }
